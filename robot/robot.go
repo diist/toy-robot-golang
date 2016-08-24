6fb8d043dc9bea7a0687617910f035e35251c143
clabbe <clement.labbe@rea-group.com>
2016-08-24 16:43:34 +1000

    MOVE

--- robot/robot.go
+++ robot/robot.go
@@ -20,6 +20,25 @@ func Place(x int, y int, direction string) (*Robot, error) {
 	return &Robot{x, y, direction}, nil
 }
 
+func Move(robot *Robot) *Robot {
+	x, y, direction := robot.X, robot.Y, robot.Direction
+
+	switch direction {
+	case "NORTH":
+		y += 1
+	case "EAST":
+		x += 1
+	case "SOUTH":
+		y -= 1
+	case "WEST":
+		x -= 1
+	}
+	if !isValidPosition(x, y) {
+		return robot
+	}
+	return &Robot{x, y, direction}
+}
+
 func isValidPosition(x int, y int) bool {
 	table := table.NewTable()
 	if x > table.MaxX || x < 0 || y > table.MaxY || y < 0 {
--- robot/robot_test.go
+++ robot/robot_test.go
@@ -31,3 +31,25 @@ func TestPlaceBadDirection(t *testing.T) {
 		t.Errorf("Robot should be nil")
 	}
 }
+
+func TestMove(t *testing.T) {
+	robot, err := Place(0, 0, "NORTH")
+	if err != nil {
+		t.Errorf("There should be no error")
+	}
+	robot = Move(robot)
+	if robot.Y != 1 {
+		t.Errorf("Robot should have moved one unit north")
+	}
+}
+
+func TestMoveEdge(t *testing.T) {
+	robot, err := Place(4, 0, "EAST")
+	if err != nil {
+		t.Errorf("There should be no error")
+	}
+	robot = Move(robot)
+	if robot.X != 4 {
+		t.Errorf("Robot should not move")
+	}
+}
